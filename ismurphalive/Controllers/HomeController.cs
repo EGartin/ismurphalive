4163f1c1da5bbf43a42932634bcef819bfc1ae5d
Elijah Gartin <elijah.gartin@thunder-rock.com>
2018-06-15 12:07:31 -0600

    Updating site for ease of reading and more needed concern lasagna

--- ismurphalive/Controllers/HomeController.cs
+++ ismurphalive/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace ismurphalive.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Is Murph alive? We're concerned.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Let us know if you know.";
 
             return View();
         }
